1e901f782f45f8cc59b16c5d99b50c6a438a2340
Roshan Suntharan <roshan.suntharan@mail.utoronto.ca>
2020-05-27 17:09:50 -0400

    Added some input code, error handling soon

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from minionValues.miningMinions import miningMinions as mm
 from minionValues.combatMinions import combatMinions as cm
 from minionValues.foragingMinions import treeMinions as tm
 from minionValues.otherMinions import otherMinions as om
+import json
 
 #print(fm)
 #print(mm)
@@ -10,4 +11,42 @@ from minionValues.otherMinions import otherMinions as om
 #print(tm)
 #print(om)
 
-print(fm["wheat"])
+allMinions = dict()
+allMinions["farming"] = fm
+allMinions["mining"] = mm
+allMinions["combat"] = cm
+allMinions["tree"] = tm
+allMinions["other"] = om
+
+counter = 1
+print("From the following, choose the category of minions you're using:")
+for keys in allMinions:
+  print(f'{counter}. {keys}')
+  counter += 1
+category = input("Make your pick: ")
+
+chosenSet = allMinions[category]
+
+counter = 1
+print("Which type of minion will you use?:")
+for keys in chosenSet:
+  print(f'{counter}. {keys}')
+  counter += 1
+minion = input("Make your pick: ")
+chosen = chosenSet[minion]
+tier = input("What tier of this minion?: ")
+tier = int(tier)
+quantity = input("How many of these minions are you using?: ")
+
+actionTime = chosen["actionTimes"][(tier-1)//2]
+unitPrice = chosen["unitPrice"]
+
+print(chosen)
+print(f'The action time for {minion} is {actionTime} seconds ' +
+      f'and its unit price is {unitPrice} coins.')
+
+# value = input("Please enter a bopbop:\n")
+# print(value)
+
+# Earnings = 86,400 / Action Time * Items Per Action * Unit Price + 138,240 / Action Time
+# Bonus Speed = Base Speed * (1 - (speed bonus sum / (1 + speed bonus sum)))
