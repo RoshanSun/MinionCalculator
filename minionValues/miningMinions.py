d234abb12c9b30f930a0cbb6048316522fec7ed8
Roshan Suntharan <roshan.suntharan@mail.utoronto.ca>
2020-05-27 23:31:35 -0400

    some more per items value added, not all done

--- minionValues/miningMinions.py
+++ minionValues/miningMinions.py
@@ -4,68 +4,83 @@ minionString = '''
 {
   "cobble": {
     "actionTimes": [14, 12, 10, 9, 8, 7],
-    "unitPrice": 5
+    "unitPrice": 5,
+    "itemsPer": 0.5
   },
   "coal": {
     "actionTimes": [15, 13, 12, 10, 9, 7],
-    "unitPrice": 10.6
+    "unitPrice": 10.6,
+    "itemsPer": 0.5
   },
   "iron": {
     "actionTimes": [17, 15, 14, 12, 10, 8],
-    "unitPrice": 5.6
+    "unitPrice": 5.6,
+    "itemsPer": 0.5
   },
   "gold": {
     "actionTimes": [22, 20, 18, 16, 14, 11],
-    "unitPrice": 11
+    "unitPrice": 11,
+    "itemsPer": 0.5
   },
   "diamond": {
     "actionTimes": [29, 27, 25, 22, 19, 15],
-    "unitPrice": 7.3
+    "unitPrice": 7.3,
+    "itemsPer": 0.5
   },
   "lapis": {
     "actionTimes": [29, 27, 25, 23, 21, 18],
-    "unitPrice": 3.3
+    "unitPrice": 3.3,
+    "itemsPer": 3.5
   },
   "emerald": {
     "actionTimes": [28, 26, 24, 21, 18, 14],
-    "unitPrice": 6
+    "unitPrice": 6,
+    "itemsPer": 0.5
   },
   "redstone": {
     "actionTimes": [29, 27, 25, 23, 21, 18],
-    "unitPrice": 2.4
+    "unitPrice": 2.4,
+    "itemsPer": 2.25
   },
   "quartz": {
     "actionTimes": [22.5, 21, 19, 17, 14.5, 11.5],
-    "unitPrice": 19.3
+    "unitPrice": 19.3,
+    "itemsPer": 0.5
   },
   "obsidian": {
     "actionTimes": [45, 42, 39, 35, 30, 24],
-    "unitPrice": 23.7
+    "unitPrice": 23.7,
+    "itemsPer": 0.5
   },
   "glowstone": {
     "actionTimes": [25, 23, 21, 19, 16, 13],
-    "unitPrice": 13.8
+    "unitPrice": 13.8,
+    "itemsPer": 0.5
   },
   "gravel": {
     "actionTimes": [26, 24, 22, 19, 16, 13],
-    "unitPrice": 2.2,
-    "flintPrice": 16.5
+    "unitPrice": 16.5,
+    "itemsPer": 0.5
   },
   "ice": {
     "actionTimes": [14, 12, 10, 9, 8, 7],
-    "unitPrice": 3.1
+    "unitPrice": 3.1,
+    "itemsPer": 0.5
   },
   "sand": {
     "actionTimes": [26, 24, 22, 19, 16, 13],
-    "unitPrice": 9.5
+    "unitPrice": 9.5,
+    "itemsPer": 0.5
   },
   "endstone": {
     "actionTimes": [26, 24, 22, 19, 16, 13],
-    "unitPrice": 14.9
+    "unitPrice": 14.9,
+    "itemsPer": 0.5
   },
   "snow": {
     "actionTimes": "not sure, will fill this in ASAP",
-    "unitPrice": 1.1
+    "unitPrice": 1.1,
+    "itemsPer": 0.5
   }
 }
 '''
